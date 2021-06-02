caccd6eca76bf626c8d4ede5787d97f00d78b2ed
apolopino <apolo.pino@gmail.com>
2021-06-02 01:00:25 +0000

    desarrollado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pinta = [" spades", " clubs", " hearts", " diams"];
+  let num = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+  let randPinta = Math.floor(Math.random() * pinta.length);
+  let pintaClass = pinta.[randPinta];
+  let randNum = Math.floor(Math.random() * num.length);
+
+  this.console.log(pinta[randPinta]);
+
+  this.document.getElementById("numero").innerHTML = num[randNum];
+
+  
+  let element = document.getElementsByClassName("pinta");
+  for (let i = 0 ; i < element.length ; i++) {
+      element[i].className += pinta.[randPinta];
+  }
+  
+
 };
